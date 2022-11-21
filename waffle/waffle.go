cc10fc3c7393324947f69aaa561d56c711bb7c43
conor <conor@cj-erazer.cjnet>
2022-11-21 22:32:59 +0000

    some type based refactoring

--- waffle/waffle.go
+++ waffle/waffle.go
@@ -4,56 +4,88 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"reflect"
 
 	"github.com/Conor-Moran/waffle/utils"
 )
 
-func parseJson(filePath string) (jsonAsMap map[string]any) {
-	jsonAsMap = make(map[string]any)
+type Recipe map[string]string
 
-	data, err := os.ReadFile(filePath)
-	utils.IfErrLogFatal(err)
+type JsonAsMap map[string]any
 
-	err = json.Unmarshal(data, &jsonAsMap)
-	utils.IfErrLogFatal(err)
+type Cutter string
 
+func parseJson(filePath string) (jsonAsMap JsonAsMap) {
+	jsonAsMap = linkedhashmap.new(type)
+
+	parseJsonX(filePath, &jsonAsMap)
 	return
 }
 
-func parseCutter(filePath string) map[string]string {
+func parseRecipe(filePath string) (recipe Recipe) {
+	recipe = make(Recipe)
+	parseJsonX(filePath, &recipe)
+	return
+}
+
+func parseJsonX[T Recipe | JsonAsMap](filePath string, jsonMap *T) {
 	data, err := os.ReadFile(filePath)
 	utils.IfErrLogFatal(err)
 
-	return map[string]string{filePath: string(data)}
+	err = json.Unmarshal(data, jsonMap)
+	utils.IfErrLogFatal(err)
 }
 
-func readFiles[T any](dirPath string, parse func(string) T) (parsed []T) {
-	parsed = make([]T, 0)
+func parseCutter(filePath string) Cutter {
+	data, err := os.ReadFile(filePath)
+	utils.IfErrLogFatal(err)
+	return Cutter(data)
+}
+
+func readFiles[T any](dirPath string, parse func(string) T) (parsed map[string]T) {
+	parsed = map[string]T{}
 
 	entries, err := os.ReadDir(dirPath)
 	utils.IfErrLogFatal(err)
 
 	for _, entry := range entries {
 		if !entry.IsDir() {
-			parsed = append(parsed, parse(utils.FilePath(dirPath, entry)))
+			filePath := utils.FilePath(dirPath, entry)
+			parsed[filePath] = parse(filePath)
 		}
 	}
 
 	return
 }
 
-func readJsonFiles(dirPath string) []map[string]any {
+func readRecipes(dirPath string) map[string]Recipe {
+	return readFiles(dirPath, parseRecipe)
+}
+
+func readJsonFiles(dirPath string) map[string]JsonAsMap {
 	return readFiles(dirPath, parseJson)
 }
 
-func readCutterFiles(dirPath string) []map[string]string {
+func readCutterFiles(dirPath string) map[string]Cutter {
 	return readFiles(dirPath, parseCutter)
 }
 
+// Run the Waffle
 func Run() {
-	recipes := readJsonFiles("./ins/recipes")
+	recipes := readRecipes("./ins/recipes")
 	ingredients := readJsonFiles("./ins/ingredients")
 	cutters := readCutterFiles("./ins/cutters")
 
-	fmt.Println(recipes, ingredients, cutters)
+	for _, x := range recipes {
+		ingredientsKey := x["ingredients"]
+		context := ingredients[ingredientsKey]
+
+		for key, value := range context {
+			fmt.Printf("\n%s -- %v", key, reflect.TypeOf(value))
+		}
+
+		fmt.Printf("\nXXX: %v\n", context)
+	}
+
+	fmt.Println(cutters)
 }
